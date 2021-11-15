fe4d2f86d7efd056c28a5f72c341cce75aaddd39
Jon Barber <jon.barber@acm.org>
2021-11-15 16:19:08 +0000

    Map for JSON response

--- cmd/api/healthcheck_test.go
+++ cmd/api/healthcheck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/matryer/is"
 	"net/http"
 	"net/http/httptest"
@@ -18,6 +19,23 @@ func TestGETHealthcheck(t *testing.T) {
 	w := httptest.NewRecorder()
 	srv.ServeHTTP(w, req)
 	is.Equal(w.Code, http.StatusOK)
-	is.Equal(w.Body.String(), "{\"status\": \"available\", \"environment\": \"development\", \"version\": \"1.0.0\"}")
+
+	type statusResponse struct {
+		Environment string `json:"environment"`
+		Status      string `json:"status"`
+		Version     string `json:"version"`
+	}
+
+	want := statusResponse{
+		Environment: "development",
+		Status:      "available",
+		Version:     "1.0.0",
+	}
+
+	got := statusResponse{}
+	err = json.Unmarshal(w.Body.Bytes(), &got)
+	is.NoErr(err)
+
+	is.Equal(want, got)
 
 }
